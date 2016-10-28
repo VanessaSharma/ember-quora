7720cc4e49ceeac178f1077837ec47ba20f4252f
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-28 09:36:51 -0700

    Add question-detail component and link question route and question-detail component to index.

---
+++ app/components/question-detail.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+//   actions: {
+//     delete(question) {
+//       if (confirm('Bye bye question?😭👋')) {
+//         this.sendAction('destroyQuestion', question);
+//       }
+//     }
+//   }
+// });
