0db47e3c6adad397cacbda1411c1ac083125a09d
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-10-28 08:41:02 -0700

    Create new questions.json file in order to display data on web app correctly.

--- config/environment.js
+++ config/environment.js
@@ -10,18 +10,16 @@ module.exports = function(environment) {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
-      },
-      EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false
       }
     },
-  firebase: {
-    apiKey: "AIzaSyB0Ex1vbay_BmYwE8Qu_5yw5lQqUdiMy6Q",
-    authDomain: "quora-app-81164.firebaseapp.com",
-    databaseURL: "https://quora-app-81164.firebaseio.com",
-    storageBucket: "quora-app-81164.appspot.com",
-  },
+
+    firebase: {
+      apiKey: "AIzaSyB0Ex1vbay_BmYwE8Qu_5yw5lQqUdiMy6Q",
+      authDomain: "quora-app-81164.firebaseapp.com",
+      databaseURL: "https://quora-app-81164.firebaseio.com",
+      storageBucket: "quora-app-81164.appspot.com"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
