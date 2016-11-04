8913db6d246f711e39c1bf72a100b65c8f3777e4
Vanessa Palacios Sharma <vanelunapalacios@live.com>
2016-11-04 08:25:46 -0700

    Add computed property for display of answers.

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,6 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  heading: Ember.computed('answer.reply', 'answer.responder', function() {
+    return this.get('answer.reply') + ' ' + '-' + ' ' +  this.get('answer.responder');
+  }),
+
   actions: {
     delete(answer) {
       if (confirm('Not confident about your answer?😰')) {
